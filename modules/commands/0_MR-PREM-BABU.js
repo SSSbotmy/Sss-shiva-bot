ff12699c0ea677d2a8bb93ca53d2d370e0ee1c04
Asnanansari <aslanansari84@gmail.com>
2024-08-08 23:14:30 +0530

    Update 0_MR-PREM-BABU.js

--- modules/commands/0_MR-PREM-BABU.js
+++ modules/commands/0_MR-PREM-BABU.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "prem",
+  name: "Faizu",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "PREM BABU",
@@ -20,13 +20,13 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
     var link = [
-"https://i.imgur.com/bJkwri4.jpeg",
-"https://i.imgur.com/Rd84Bhe.jpeg",
-"https://i.imgur.com/71hdujn.jpeg",
-"https://i.imgur.com/0sWGhwe.jpeg",
-"https://i.imgur.com/86kmX7S.jpeg",
-"https://i.imgur.com/jrYsBnF.jpeg"
+"https://i.imgur.com/f0NpePe.jpeg",
+"https://i.imgur.com/OLbbpyz.jpeg",
+"https://i.imgur.com/ehUSGQp.jpeg",
+"https://i.imgur.com/5HROPJS.jpeg",
+"https://i.imgur.com/7ZmMnVR.jpeg",
+"https://i.imgur.com/VSNvvDZ.jpeg"
      ];
-     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     प्रेम-बाबू                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nदेख लो मेरे बॉस प्रेम बाबू को सबसे अलग मासूम चेहरा भोली सी सूरत आखों में प्यार दिल में खुमार ♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     फैज-अंसारी                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nदेख लो मेरे फैज अंसारी बाबू को सबसे अलग मासूम चेहरा भोली सी सूरत आखों में प्यार दिल में खुमार ♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
