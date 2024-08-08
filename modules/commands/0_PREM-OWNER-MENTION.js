a1ae2ceba54be78a50fc5f448037cb50899c33a1
Asnanansari <aslanansari84@gmail.com>
2024-08-08 23:40:15 +0530

    Update 0_PREM-OWNER-MENTION.js

--- modules/commands/0_PREM-OWNER-MENTION.js
+++ modules/commands/0_PREM-OWNER-MENTION.js
@@ -9,7 +9,7 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100070531069371") {
+  if (event.senderID !== "61552656562989") {
 ];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
