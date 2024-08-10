3c4c56dd6beb91361970dd41e5ff51cd5ca73071
Asnanansari <aslanansari84@gmail.com>
2024-08-10 10:41:26 +0530

    Update 0_FAIZ-GOOD-MORNING.js

--- modules/commands/0_FAIZ-GOOD-MORNING.js
+++ modules/commands/0_FAIZ-GOOD-MORNING.js
@@ -1,29 +1,28 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "GOOD MORNING",
-    version: "2.1.1",
-  hasPermssion: 0,
-  credits: "PREM BABU", 
-  description: "THIS BOT WAS MADE BY MR PREM BABU",
-  commandCategory: "NO PREFIX",
+	name: "good morning",
+    version: "1.1.1",
+	hasPermssion: 0,
+	credits: "FAIZ ANSARI", 
+	description: "Just Respond",
+	commandCategory: "no prefix",
     cooldowns: 5, 
 };
 
-module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
-  var name = await Users.getNameUser(event.senderID);
-  var { threadID, messageID } = event;
-  let react = event.body.toLowerCase();
-  if(react.includes("good morning") ||
-     react.includes("gud morning") || react.includes("gm") || react.includes("GM") ||
-react.includes("Gm") ||
-react.includes("GOOD MORNING")) {
-    var msg = {
-        body: ``,attachment: fs.createReadStream(__dirname + `/PREM-BABU/PREM-GIF/GOOD-MORNING.gif`)
-      }
-      api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🥱", event.messageID, (err) => {}, true)
-    }
-  }
-  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("GOOD MORNING") ||
+     react.includes("good morning") || react.includes("gm") || react.includes("Gm") ||
+react.includes("morning") ||
+react.includes("MORNING")) {
+		var msg = {
+				body: `𝐕𝐄𝐑𝐘 𝐆𝐎𝐎𝐃 𝐌𝐎𝐑𝐍𝐈𝐍𝐆 𝐉𝐀𝐀𝐍😻 `,attachment: fs.createReadStream(__dirname + `/PREM-BABU/morning.gif`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😻", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
   }
