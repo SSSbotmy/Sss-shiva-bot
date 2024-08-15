86396effa103d7cbab4d0a201b7d80bfeb54af18
PFAIZBOT <shaziyashaik786786@gmail.com>
2024-08-15 23:24:46 +0530

    Update 0_PREM-PAIR-MENTION-1.js

--- modules/commands/0_PREM-PAIR-MENTION-1.js
+++ modules/commands/0_PREM-PAIR-MENTION-1.js
@@ -23,7 +23,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'ar2.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/ziew4GB.jpeg", path); 
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/axRfkJp.jpeg", path); 
 }
 
 async function makeImage({ one, two }) {
