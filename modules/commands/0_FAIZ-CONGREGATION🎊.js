d393a02f2c33aeab737513c8e44249e7e53047f5
Faizbabu <shadabshadabansar9079@gmail.com>
2024-08-21 16:16:16 +0530

    Update 0_FAIZ-CONGREGATION🎊.js

--- modules/commands/0_FAIZ-CONGREGATION🎊.js
+++ modules/commands/0_FAIZ-CONGREGATION🎊.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   if (event.body.indexOf("congratulations")==0 || event.body.indexOf("CONGREGATION")==0 || event.body.indexOf("Congratulations")==0 || event.body.indexOf("Mubarak ho")==0) {
     var msg = {
         body: "🎊𝐂𝐎𝐍𝐆𝐑𝐀𝐓𝐔𝐋𝐀𝐓𝐈𝐎𝐍𝐄🎊",
-        attachment: fs.createReadStream(__dirname + `/FAIZ/congratulations.gif`)
+        attachment: fs.createReadStream(__dirname + `/FAIZ/congratulations .gif`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🎊", event.messageID, (err) => {}, true)
