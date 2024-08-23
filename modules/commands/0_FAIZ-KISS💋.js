5c0e2ba18dee36a2e7f42f94abc19d0e8a58cb17
Faizbabu <shadabshadabansar9079@gmail.com>
2024-08-23 21:00:48 +0530

    Update 0_FAIZ-KISS💋.js

--- modules/commands/0_FAIZ-KISS💋.js
+++ modules/commands/0_FAIZ-KISS💋.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("kiss me") ||
 react.includes("KISS YOU")) {
     var msg = {
-        body: `𝑰 𝑲𝑰𝑺𝑺 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑩𝑨𝑩𝒀 💋🙊💞`,attachment: fs.createReadStream(__dirname + `/FAIZ/kissyou.gif`)
+        body: `𝑰 𝑲𝑰𝑺𝑺 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑩𝑨𝑩𝒀 💋🙊💞`,attachment: fs.createReadStream(__dirname + `/FAIZ/kissyou (1).gif`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("💋", event.messageID, (err) => {}, true)
