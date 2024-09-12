258f6e7a5a9b328fdfe5fa736e360fa1a77b56e9
𝐀𝐍𝐒𝐀𝐑𝐈-𝐇𝐔-𝐏𝐀𝐆𝐀𝐋❤🪽 <varshasingh982864@gmail.com>
2024-09-12 09:08:29 +0530

    Update 0_FAIZ-ASSALAMUALAIKUM❤️.js

--- modules/commands/0_FAIZ-ASSALAMUALAIKUM❤️.js
+++ modules/commands/0_FAIZ-ASSALAMUALAIKUM❤️.js
@@ -1,7 +1,6 @@
-const fs = require("fs");
 module.exports.config = {
-  name: "Assalamu alaikkum",
-    version: "1.1.1",
+  name: "Asalamu alaikum",
+  version: "7.3.1",
   hasPermssion: 0,
   credits: "FAIZ ANSARI", 
   description: "Just Respond",
@@ -9,15 +8,12 @@ module.exports.config = {
     cooldowns: 5, 
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
   var { threadID, messageID } = event;
-  let react = event.body.toLowerCase();
-  if(react.includes("asalamu alaikum") ||
-     react.includes("assalamu alaikkum") || react.includes("Assalamu alaikkum") || react.includes("ASALAMU ALAIKUM") ||
-react.includes("Assalamualaikum") ||
-react.includes("Assalamu Alaikum")) {
+  var name = await Users.getNameUser(event.senderID);
+  if (event.body.indexOf("asalamu alaikum")>=0 || event.body.indexOf("assalamu alaikkum")>=0 || event.body.indexOf("Assalamu alaikkum")>=0 || event.body.indexOf("ASALAMU ALAIKUM")>=0 || event.body.indexOf("Assalamualaikum")>=0 || event.body.indexOf("Assalamu Alaikum")>=0 || event.body.indexOf("Asslam alikum good moring")>=0 || event.body.indexOf("Salam")>=0 || event.body.indexOf("asalam")>=0 || event.body.indexOf("asalamualaikum friends")>=0 ) { 
     var msg = {
-        body: `♥ W'aleykum Assalam Wa'rahmatullahi Wa'barakatuh ❤️`,
+        body: ` ${name} ♥ W'aleykum Assalam Wa'rahmatullahi Wa'barakatuh ❤️`
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("❤️", event.messageID, (err) => {}, true)
